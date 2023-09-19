609ba3beac4086f7dd0b7698642a45e412e24a78
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2023-09-19 19:32:22 +0600

    Revision 1

--- task_03.php
+++ task_03.php
@@ -7,10 +7,23 @@ $series_length = 10;
 $prev_num1 = 0;
 $prev_num2 = 1;
 
-printf("First %d numbers in the Fibonacci series are:\n%d %d ", $series_length, $prev_num1, $prev_num2);
+printf("First %d numbers in the Fibonacci series are:\n", $series_length);
 
-for ($i = 3; $i <= $series_length; $i++)
+for ($i = 1; $i <= $series_length; $i++)
 {
+    if ($i == 1)
+    {
+        print("$prev_num1 ");
+
+        continue;
+    }
+    elseif ($i == 2)
+    {
+        print("$prev_num2 ");
+
+        continue;
+    }
+
     $curr_num = $prev_num1 + $prev_num2;
 
     if ($curr_num > 100) break;
--- task_04.php
+++ task_04.php
@@ -7,10 +7,23 @@ function fibonacci(int $series_length = 15): void
     $prev_num1 = 0;
     $prev_num2 = 1;
 
-    printf("First %d numbers in the Fibonacci series are:\n%d %d ", $series_length, $prev_num1, $prev_num2);
+    printf("First %d numbers in the Fibonacci series are:\n", $series_length);
 
-    for ($i = 3; $i <= $series_length; $i++)
+    for ($i = 1; $i <= $series_length; $i++)
     {
+        if ($i == 1)
+        {
+            print("$prev_num1 ");
+
+            continue;
+        }
+        elseif ($i == 2)
+        {
+            print("$prev_num2 ");
+
+            continue;
+        }
+
         $curr_num = $prev_num1 + $prev_num2;
         
         print("$curr_num ");
